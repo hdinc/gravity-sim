6c2d92eac67e7ee8e19d2ed58efb8c89d7bb3d3a
hdxx <macellan091@gmail.com>
2018-10-21 04:32:48 +0300

    added color attribute

--- main.c
+++ main.c
@@ -6,15 +6,15 @@
 #include "window.h"
 #include "shader.h"
 
-static const float positions[] = {
-    0.00f, 0.50f, //v0
-    0.25f, 0.25f, //v1
-    0.50f, 0.00f, //v2
-    0.25f, -0.25f, //v3
-    0.00f, -0.50f, //v4
-    -0.25f, -0.25f, //v5
-    -0.50f, 0.00f, //v6
-    -0.25f, 0.25f //v7
+static const float vertices[] = {
+     0.00f,  0.50f, 1.00f, 0.00f, 0.00f, //v0 vec2 position and vec3 color
+     0.25f,  0.25f, 0.00f, 1.00f, 0.00f, //v1
+     0.50f,  0.00f, 0.00f, 0.00f, 1.00f, //v2
+     0.25f, -0.25f, 1.00f, 0.00f, 0.00f, //v3
+     0.00f, -0.50f, 0.00f, 1.00f, 0.00f, //v4
+    -0.25f, -0.25f, 0.00f, 0.00f, 1.00f, //v5
+    -0.50f,  0.00f, 1.00f, 0.00f, 0.00f, //v6
+    -0.25f,  0.25f, 0.00f, 1.00f, 0.00f  //v7
 };
 
 static const unsigned int indices[] = {
@@ -35,22 +35,24 @@ int main()
     GLuint vertexbuffer;
     glGenBuffers(1, &vertexbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glBufferData(GL_ARRAY_BUFFER, 8 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 8 * 5 * sizeof(float), vertices, GL_STATIC_DRAW);
 
     GLuint indexbufer;
     glGenBuffers(1, &indexbufer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbufer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * 4 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 5 * sizeof(float));                  //burası yanlıs olabilir
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 5 * sizeof(float);
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
     GLuint shader = createShaderProgram("vertexShader.glsl", "fragmentShader.glsl");
     
 
     GLint uniformid = glGetUniformLocation(shader, "u_barzo");
     glUseProgram(shader);
-    glUniform4f(uniformid, 0.0f,0.8f,0.2f,1.0f);
+    //glUniform4f(uniformid, 0.0f,0.8f,0.2f,1.0f);
 
     startLoop();
 
